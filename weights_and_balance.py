e5d5130cec3e9f05a257802929faf25d978e2199
Jamie Cooke <jamie@iamjamiecooke.com>
2018-04-10 18:20:36 +0100

    Add loops and helper functions

--- weights_and_balance.py
+++ weights_and_balance.py
@@ -56,15 +56,44 @@ big_weights = {"MTOW_w": cg_params["Q4"].value,
 
 ax_lims=[big_weights["OWE_w"]*0.98, big_weights["MTOW_w"]*1.02]
 c_bar = cg_params["M7"].value
-print(c_bar)
 h0 = cg_params["M8"].value
+print(h0)
+wing_pos = h0 * c_bar
+seat_start_fwd = 6.304  # m
+seat_start_aft = 21.290  # m
+seat_pitch = 31 * 2.54 / 100
+# Luggage
+front_hold_front = 0  # m
+
+
+def mac_x_point(mac_pos, w):
+    return (mac_pos - 0.25) * w
 
 
 def mac_axes(mac_pos):
-    lower_ax_point = (mac_pos-0.25) * ax_lims[0]
-    upper_ax_point = (mac_pos-0.25) * ax_lims[1]
-    print(lower_ax_point)
+    lower_ax_point = mac_x_point(mac_pos, ax_lims[0])
+    upper_ax_point = mac_x_point(mac_pos, ax_lims[1])
     return [lower_ax_point, upper_ax_point]
+
+
+def mom_calc(w, x):
+    print(x)
+    print(h0)
+    print(x/c_bar)
+    print(((x/c_bar) - h0))
+    return w * ((x/c_bar) - h0)
+
+
+def seat_loading_wa(seats=2):
+    distances = []
+    moms = []
+    pax_mass = (95-23) * seats  # For both window seats
+    for row in np.arange(1, 19, 1):
+        local_dist = ((seat_start_fwd + ((row - 1) * seat_pitch))/c_bar) - h0
+        distances.append(local_dist)
+        moms.append(pax_mass * local_dist)
+    return {"distances": distances, "moments": moms, "pax mass": pax_mass}
+
 
 
 def to_tons(x, pos):
@@ -76,11 +105,14 @@ def to_mac(x, pos):
     return str(int(round(100*(x/ax_lims[0]+0.25)))) + '%'
 
 
+
+
+
 formattery = FuncFormatter(to_tons)
 formatterx = FuncFormatter(to_mac)
 
 
-def plotit(mac_range=[0.11,0.51]):
+def plotit(mac_range=[0.11, 0.51]):
     fig = plt.figure(figsize=(10, 7))
     ax = fig.add_subplot(1, 1, 1)
     ax.yaxis.set_major_formatter(formattery)
@@ -88,7 +120,7 @@ def plotit(mac_range=[0.11,0.51]):
     ax.xaxis.set_ticks_position('bottom')
     ax.yaxis.grid(which="major", color='k', linestyle='-', linewidth=1)
 
-    # Plot MAC axes
+    """ Plot MAC axes """
     mac_spacing = 0.02
     mac_set = np.arange(mac_range[0], mac_range[1], mac_spacing)
 
@@ -98,6 +130,43 @@ def plotit(mac_range=[0.11,0.51]):
         xtick_hold.append(mac_axes(mac_pos)[0])
         # plt.annotate(mac_pos, [mac_axes(mac_pos)[0], ax_lims[1]-(0.05*(ax_lims[1]-ax_lims[0]))])
     plt.xticks(xtick_hold)
+
+    """ This section plots static points"""
+    # OWE point from CG Excel
+    curr_weight = big_weights["OWE_w"]
+    owe_mom = mom_calc(big_weights["OWE_w"], big_weights['OWE_x'])
+    plt.plot(owe_mom, curr_weight, 'ro', markersize=10)
+
+
+    """ This section plots the loops front first"""
+    curr_mom = owe_mom
+    seat_fwd = seat_loading_wa(2)
+    seat_window_loop_moms = [curr_mom]
+    seat_window_loop_weights = [curr_weight]
+    for row_id, row in enumerate(seat_fwd["moments"]):
+        curr_weight = curr_weight + seat_fwd["pax mass"]
+        curr_mom = curr_mom + row
+        seat_window_loop_moms.append(curr_mom)
+        seat_window_loop_weights.append(curr_weight)
+
+    for row_id, row in enumerate(seat_fwd["moments"]):
+        curr_weight = curr_weight + seat_fwd["pax mass"]
+        curr_mom = curr_mom + row
+        seat_window_loop_moms.append(curr_mom)
+        seat_window_loop_weights.append(curr_weight)
+
+    seat_fwd = seat_loading_wa(1)
+    for row_id, row in enumerate(seat_fwd["moments"]):
+        curr_weight = curr_weight + seat_fwd["pax mass"]
+        curr_mom = curr_mom + row
+        seat_window_loop_moms.append(curr_mom)
+        seat_window_loop_weights.append(curr_weight)
+
+    # Plot Window Loop
+    plt.plot(seat_window_loop_moms, seat_window_loop_weights, 'b')
+
+
+
     plt.ylabel("Mass")
     plt.xlabel("Moment (temp)")
     plt.xlim(mac_axes(mac_range[0]-0.01)[0], mac_axes(mac_range[1]+0.01)[0])
@@ -105,4 +174,4 @@ def plotit(mac_range=[0.11,0.51]):
     plt.show()
 
 plotit()
-print(to_mac(443,0))
+

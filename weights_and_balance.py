3eb1ad9ba05425c55f009b1e30be91480be176e6
Jamie Cooke <jamie@iamjamiecooke.com>
2018-05-03 15:11:05 +0100

    Tidy and fix excel references.

--- weights_and_balance.py
+++ weights_and_balance.py
@@ -160,7 +160,8 @@ def fuel_loading():
 
 def tank_pos(ob_dist):
     tank_centre_c_pos = 0.4
-    tank_centre_sweep = np.arctan((0.5-tank_centre_c_pos)/(cad_file['Interface-80']['B85'].value/2))
+    tank_centre_sweep = np.arctan(
+        (cad_file['Interface-80']['B101'].value * (0.5 - tank_centre_c_pos)) / (cad_file['Interface-80']['B98'].value / 2))
     # print("tank sweep: " + str(tank_centre_sweep))
     offset = np.tan(tank_centre_sweep) * ob_dist
     total_pos = offset + cad_file['Sheet2']['S3'].value
